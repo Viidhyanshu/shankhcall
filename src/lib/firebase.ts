0cb38ca12048699bb59068ecf243421d9a4d1fbc
Vidhyanshu Kumar <vidhyanshukumarvidhyam@gmail.com>
2026-06-16 19:19:12 +0530

    chore: move Firebase config to environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAf3sxtgaLYjuhUOepK5Hljm2kI4BtEKxg",
-  authDomain: "shankhcall.firebaseapp.com",
-  projectId: "shankhcall",
-  storageBucket: "shankhcall.firebasestorage.app",
-  messagingSenderId: "797642858514",
-  appId: "1:797642858514:web:13970ef7e1243848f278cb"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
